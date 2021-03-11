e460aeeb6be12730f282d3c5156604d75c5edaae
Damean Rittmann <46982345+drittmann19@users.noreply.github.com>
2021-03-11 13:20:16 -0700

    parallax box working

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,27 @@
 const path = require("path");
 
 module.exports = {
-    mode: "development",
-    entry: "./src/index.js",
-    output: {
-      filename: "main.js",
-      path: path.resolve(__dirname, "dist")
-    },
-    module: {
-      rules: [{
-            test: /\.scss$/,
-            use: ["style-loader", // 3. injects css into DOM
-            "css-loader", // 2.turns css into commonjs
-            "sass-loader", // 1.turns sass into css
-            ],
-        }, ],
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", // 3. injects css into DOM
+          "css-loader", // 2.turns css into commonjs
+          "sass-loader", // 1.turns sass into css
+        ],
       },
-    };
+      {
+        test: /\.(png|jpg)$/,
+        use: [
+          {loader: 'url-loader'}
+        ]
+      }
+  ],
+  },
+};
